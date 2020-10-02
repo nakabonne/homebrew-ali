e69b5f8e06383cb118afb739e88d6a35d256156e
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-02 14:34:45 +0900

    Brew formula update for ali version v0.3.1

--- ali.rb
+++ ali.rb
@@ -2,24 +2,24 @@
 class Ali < Formula
   desc "Generate HTTP load and plot the results in real-time"
   homepage "https://github.com/nakabonne/ali"
-  version "0.3.0"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nakabonne/ali/releases/download/v0.3.0/ali_0.3.0_darwin_amd64.tar.gz"
-    sha256 "8d884f0bff94afed47546fcbfb7c165a1d997dda1fa501dc6d8a58e045cadf29"
+    url "https://github.com/nakabonne/ali/releases/download/v0.3.1/ali_0.3.1_darwin_amd64.tar.gz"
+    sha256 "1fe93d9e51ec62b93901f80421fdfd44691eba6e732c221e67daa27b4eb0cc03"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nakabonne/ali/releases/download/v0.3.0/ali_0.3.0_linux_amd64.tar.gz"
-      sha256 "1ad50aed67817ea048377d2144d88cf08ddc5e8a253d9ec4fed11a52b5b47851"
+      url "https://github.com/nakabonne/ali/releases/download/v0.3.1/ali_0.3.1_linux_amd64.tar.gz"
+      sha256 "62e0f01a8a66098c978330da6d3e09d1005eef94c84712dbd18b6f3fd41680de"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nakabonne/ali/releases/download/v0.3.0/ali_0.3.0_linux_arm64.tar.gz"
-        sha256 "5d29124a45ce2500bb3f35aa0908bca9e67b89011a69bab60db08823367ce259"
+        url "https://github.com/nakabonne/ali/releases/download/v0.3.1/ali_0.3.1_linux_arm64.tar.gz"
+        sha256 "bae4588335014e602fb69e392f7c5a8937b4a961bc7b09bfcf0647e1070dd5c0"
       else
-        url "https://github.com/nakabonne/ali/releases/download/v0.3.0/ali_0.3.0_linux_armv6.tar.gz"
-        sha256 "0ef17f32a0af7757b49ccc317c13a85511a0c94448393edbf403b111b7e3e337"
+        url "https://github.com/nakabonne/ali/releases/download/v0.3.1/ali_0.3.1_linux_armv6.tar.gz"
+        sha256 "efa637d1aaf31f2ad404a89fe8935d49cfc02172e7832baaba8f70b40a6e3f23"
       end
     end
   end
